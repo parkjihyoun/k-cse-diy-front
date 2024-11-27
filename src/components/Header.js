5b66b7a2c635853a4cc580c6f8dc341acc7395d0
wnhaoo <153691178+wnhaoo@users.noreply.github.com>
2024-11-27 20:02:22 +0900

    메인페이지1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+    const location = useLocation();
+
     return (
         <header style={styles.header}>
-            <div style={styles.logo}>K:DIY</div>
+            <div style={styles.logo}>
+                <Link to="/" style={styles.logoLink}>K:DIY</Link>
+            </div>
 
             <nav style={styles.nav}>
                 <ul style={styles.navList}>
                     <li style={styles.navItem}>
-                        <Link to="/" style={styles.navLink}>메인</Link>
+                        <Link to="/" style={styles.navLink}>
+                            메인
+                            {location.pathname === '/' && <div style={styles.activeLine}></div>}
+                        </Link>
                     </li>
                     <li style={styles.navItem}>
-                        <Link to="/month" style={styles.navLink}>예약하기</Link>
+                        <Link to="/month" style={styles.navLink}>
+                            예약하기
+                            {location.pathname === '/month' && <div style={styles.activeLine}></div>}
+                        </Link>
                     </li>
                     <li style={styles.navItem}>
-                        <Link to="/week" style={styles.navLink}>주</Link>
+                        <Link to="/week" style={styles.navLink}>
+                            주
+                            {location.pathname === '/week' && <div style={styles.activeLine}></div>}
+                        </Link>
                     </li>
                     <li style={styles.navItem}>
-                        <Link to="/key" style={styles.navLink}>열쇠 대여/반납</Link>
+                        <Link to="/key" style={styles.navLink}>
+                            열쇠 대여/반납
+                            {location.pathname === '/key' && <div style={styles.activeLine}></div>}
+                        </Link>
                     </li>
                     <li style={styles.navItem}>
-                        <Link to="/check" style={styles.navLink}>예약 확인/수정</Link>
+                        <Link to="/check" style={styles.navLink}>
+                            예약 확인/수정
+                            {location.pathname === '/check' && <div style={styles.activeLine}></div>}
+                        </Link>
                     </li>
                     <li style={styles.navItem}>
-                        <Link to="/help" style={styles.navLink}>이용 안내</Link>
+                        <Link to="/help" style={styles.navLink}>
+                            이용 안내
+                            {location.pathname === '/help' && <div style={styles.activeLine}></div>}
+                        </Link>
                     </li>
                 </ul>
             </nav>
@@ -36,22 +58,17 @@ const styles = {
     header: {
         display: 'flex',
         color: '#fff',
-        // position: 'fixed',
+        position: 'fixed',
         width: '100%',
         alignItems: 'center',
         justifyContent: 'space-between',
         padding: '30px 40px',
-        // backgroundColor: 'rgba(0, 0, 0, 0.7)',
     },
     logo: {
         fontSize: '30px',
         fontWeight: '700',
     },
-
-    nav: {
-        marginRight: '80px',
-        
-    },
+    nav: {},
     navList: {
         display: 'flex',
         listStyle: 'none',
@@ -61,6 +78,10 @@ const styles = {
     },
     navItem: {
         margin: 0,
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
     },
     navLink: {
         color: '#fff',
@@ -68,9 +89,16 @@ const styles = {
         fontSize: '15px',
         fontWeight: '500',
     },
-    navLinkHover: {
-        color: '#00aced',
+    activeLine: {
+        position: 'absolute',
+        bottom: '-10px',
+        width: '120%',
+        height: '1px',
+        backgroundColor: '#fff',
+        left: '50%',
+        transform: 'translateX(-50%)',
     },
 };
+
 
 export default Header;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import '../styles/MainPage.css';
 
 const MainPage = () => {
-  return <div>This is the Main Page</div>;
+  return (
+    <div className="main-container">
+      <div className= "main-text">
+        <h1 className="main-title">KNU CSE DIY Reservation Services</h1>
+        <p className="main-subtitle">KNU CSE DIY실 예약 서비스</p>
+        <button className="main-button">지금 예약하기</button>
+      </div>
+
+      <div className="main-status">
+        <div className="status-title-wrapper">
+          <div className="status-title">실시간 예약 현황</div>
+          <div className="status-line"></div>
+        </div>
+        <div className="status-box">
+          <div className="status">
+            <h3>2024.11.14 THU</h3>
+            <p>12:00 - 14:00</p>
+            <div className="status-info-line"></div>
+            <p>호예찬</p>
+            <p>산사랑 연극 연습</p>
+          </div>
+          <div className="status">
+            <h3>2024.11.16 SAT</h3>
+            <p>17:00 - 20:00</p>
+            <div className="status-info-line"></div>
+            <p>최원아</p>
+            <p>할거있음</p>
+          </div>
+          <div className="status">
+            <h3>2024.11.18 MON</h3>
+            <p>09:00 - 11:30</p>
+            <div className="status-info-line"></div>
+            <p>최예윤</p>
+            <p>교수님 면담</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
